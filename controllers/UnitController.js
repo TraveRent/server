3298ee66a8e9a8bbe2557f60d30b9fbdc282f842
farhanhrywn <farhanharyawan@gmail.com>
2020-12-13 23:33:19 +0700

    dev-complete-unitController

--- controllers/UnitController.js
+++ controllers/UnitController.js
@@ -3,13 +3,14 @@ const { Unit } = require('../models')
 module.exports = class UnitController {
   static async postAddVendorUnit(req, res, next) {
     try {
-      const { name, brand, type, year, category } = req.body
+      const { name, brand, type, year, category, imageUrl } = req.body
       const newUnit = new Unit({
         name: name,
         brand: brand,
         type: type,
         year: year,
-        category: category
+        category: category,
+        imageUrl: imageUrl
       })
 
       const createdUnit = await newUnit.save()
@@ -17,6 +18,45 @@ module.exports = class UnitController {
       res.status(201).json(createdUnit)
     } catch (err) {
       next(err)
+    }
+  }
+
+  static async getAllUnit(req, res, next) {
+    try {
+      const allUnit = await Unit.find({})
+      res.status(200).json(allUnit)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async editUnitById(req, res, next) {
+    try {
+      const id = req.params.id
+      const { name, brand, type, year, category, imageUrl } = req.body
+      const newUnit = new Unit({
+        name: name,
+        brand: brand,
+        type: type,
+        year: year,
+        category: category,
+        imageUrl: imageUrl
+      })
+
+      const updatedUnit = await Unit.findByIdAndUpdate(id, newUnit, { returnOriginal: false })
+      res.status(200).json(updatedUnit)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async deleteUnitById(req, res, next) {
+    try {
+      const id = req.params.id
+      const deletedUnit = await Unit.findByIdAndDelete(id)
+      res.status(200).json(deletedUnit)
+    } catch (error) {
+      next(error)
     }
   }
 }
