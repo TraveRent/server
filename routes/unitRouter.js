b31d20e0dbb2b7fc31c714f3c0541fc7ddbacb4c
itsagra <ftrahl@gmail.com>
2020-12-16 16:47:36 +0700

    select profile page done

--- routes/unitRouter.js
+++ routes/unitRouter.js
@@ -1,16 +1,20 @@
-const router = require('express').Router()
-const { UnitController } = require('../controllers')
+const router = require("express").Router();
+const { UnitController } = require("../controllers");
 
-const { authentication, authorization } = require('../middlewares')
-const { uploadVendorUnitMiddleware } = require('../aws')
+const { authentication, authorization } = require("../middlewares");
+const { uploadVendorUnitMiddleware } = require("../aws");
 
-router.use(authentication)
-router.get('/', UnitController.getAllVendorUnit)
-router.post('/add', uploadVendorUnitMiddleware, UnitController.postAddVendorUnit)
-router.get('/:unitId', UnitController.getVendorUnitById)
+router.get("/", UnitController.getAllVendorUnit);
+router.use(authentication);
+router.post(
+  "/add",
+  uploadVendorUnitMiddleware,
+  UnitController.postAddVendorUnit
+);
+router.get("/:unitId", UnitController.getVendorUnitById);
 
-router.use('/:unitId', authorization)
-router.put('/:unitId', UnitController.putEditVendorUnitById)
-router.delete('/:unitId', UnitController.deleteVendorUnitById)
+router.use("/:unitId", authorization);
+router.put("/:unitId", UnitController.putEditVendorUnitById);
+router.delete("/:unitId", UnitController.deleteVendorUnitById);
 
-module.exports = router
+module.exports = router;
